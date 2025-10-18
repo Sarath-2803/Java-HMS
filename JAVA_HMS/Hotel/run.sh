e7e10d4d98d93481d8ccee5f8ed770828f6323c7
Sarath-2803 <sarathkrishna357@gmail.com>
2025-10-18 20:13:00 +0530

    added mig dir env variable

--- JAVA_HMS/Hotel/run.sh
+++ JAVA_HMS/Hotel/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set environment variables
-export MIG_DIR="/home/sarath/Projects/oop/Java-HMS/JAVA_HMS/Hotel/resources/db/migrations/";
-export DB_USER=postgres
-export DB_PASSWORD=postgres
-export DB_URL=jdbc:postgresql://localhost:5432/test
+export MIG_DIR="/home/sarath/Projects/oop/Java-HMS/JAVA_HMS/Hotel/resources/db/migrations/"; # Set your migrations directory
+export DB_USER=postgres     # Set your database username
+export DB_PASSWORD=postgres  # Set your database password
+export DB_URL=jdbc:postgresql://localhost:5432/test  # Set your database URL
 
 # Create bin folder if it doesn't exist
 mkdir -p bin

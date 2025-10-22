f02adf51dc5f296305397e9a4bb623962a4d204f
Sarath Krishna S <sarathkrishna357@gmail.com>
2025-10-22 18:29:42 +0530

    comments removed

--- JAVA_HMS/Hotel/resources/db/migrations/V6_Room_table_created.sql
+++ JAVA_HMS/Hotel/resources/db/migrations/V6_Room_table_created.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS rooms (
     id BIGSERIAL PRIMARY KEY,
-    roomNumber BIGSERIAL NOT NULL UNIQUE,  -- From 1st
-    type VARCHAR(100) NOT NULL,              -- From 1st  
-    price DECIMAL(10,2) NOT NULL,           -- From 1st (better for money)
-    capacity INTEGER NOT NULL,               -- From 1st
-    available BOOLEAN DEFAULT TRUE          -- From 2nd
+    roomNumber BIGSERIAL NOT NULL UNIQUE, 
+    type VARCHAR(100) NOT NULL,              
+    price DECIMAL(10,2) NOT NULL,          
+    capacity INTEGER NOT NULL,               
+    available BOOLEAN DEFAULT TRUE         
 );
